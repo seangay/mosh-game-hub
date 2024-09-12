5e94f1fd96cbd8093523780595e9936b461c1385
Sean Gay <sean.gay@gmail.com>
2024-09-12 18:00:46 +1000

    update page title
     remove redundant console logging

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,6 @@ import logo from "../assets/logo.webp";
 import ColorModeSwich from "./ColorModeSwich";
 
 const NavBar = () => {
-  console.log("API key:", import.meta.env);
   return (
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
