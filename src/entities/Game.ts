88de843385baf65481ec38ac3cadf8d75a9f93b0
Sean Gay <sean.gay@gmail.com>
2024-09-16 13:55:50 +1000

    feat: build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,11 +1,15 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
   background_image: string;
   description_raw: string;
+  genres: Genre[];
   parent_platforms: { platform: Platform }[];
+  publishers: Publisher[];
   metacritic: number;
   rating_top: number;
   slug: string;
