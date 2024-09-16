06c566a4b6f3ba596b9c1bee8a8eec5f66901cc9
Sean Gay <sean.gay@gmail.com>
2024-09-16 14:34:37 +1000

    refactor entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,10 +1,10 @@
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
-import { Game } from "../entities/Game";
-import PlatformIIconList from "./PlatformIIconList";
-import CriticScore from "./CriticScore";
-import getCroppedImageUrl from "../services/image-url";
-import Emoji from "./Emoji";
 import { Link } from "react-router-dom";
+import Game from "../entities/Game";
+import getCroppedImageUrl from "../services/image-url";
+import CriticScore from "./CriticScore";
+import Emoji from "./Emoji";
+import PlatformIIconList from "./PlatformIIconList";
 
 interface Props {
   game: Game;
